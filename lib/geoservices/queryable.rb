48f0aec75d6304345737234e27f9e8c0b6438019
Rohit Kumar <rohitz1990@gmail.com>
2024-03-06 20:09:18 +0530

    PR feedback

--- lib/geoservices/queryable.rb
+++ lib/geoservices/queryable.rb
@@ -12,17 +12,17 @@ module Geoservice
 
     # Returns the # of features expected from a query call for the Geoservice
     # returns nil if the layer does not have the Capability: Query
-    def count(layer_id = 0, options = {})
+    def count(layer_idx = 0, options = {})
       return nil unless metadata['capabilities'] =~ /Query/
       # Current Version is not returned at lower than 10.1
       if !metadata['currentVersion'].nil?
-        query(layer_id, options.merge(returnCountOnly: true))
+        query(layer_idx, options.merge(returnCountOnly: true))
       else
         # Neither returnCountOnly nor currentVersion are supported for
         # layers @ lower than 10.0 SP1
         # http://resources.arcgis.com/en/help/rest/apiref/
         # So request all the object ids and count them up
-        response = query(layer_id, options.merge(where: '1=1',
+        response = query(layer_idx, options.merge(where: '1=1',
                                                  returnIdsOnly: true))
         { 'count' => response['objectIds'].length }
       end
